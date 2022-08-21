2c929227bbc1e963b22c0018a916aa61634cea62
Jean Baez <jlbaez44@gmail.com>
2022-08-21 19:57:50 -0400

    se agrego crear editar y eliminar empleos
    Signed-off-by: Jean Baez <jlbaez44@gmail.com>

---
+++ Web/BolsaDeEmpleo.Client/BolsaDeEmpleo.Client/Controllers/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BolsaDeEmpleo.Client.Controllers
+{
+    internal class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+    }
+}
--- Web/BolsaDeEmpleo.Client/BolsaDeEmpleo.Client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BolsaDeEmpleo.Client.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
